9fa138e1e6a1484ba4d4274d92b3a13a0deadfd6
kurumimi <foreverlove0512t@gmail.com>
2025-04-28 22:28:26 +0800

    250428 22:28

--- src/main/java/cart/controller/IndexServlet.java
+++ src/main/java/cart/controller/IndexServlet.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/")
 public class IndexServlet extends HttpServlet {
-
+	//跑首頁的index
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.getRequestDispatcher("/WEB-INF/view/cart/index.jsp").forward(req, resp);
